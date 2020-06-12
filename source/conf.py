9f7d50557b2a921ad4f5a5e2e1449b4faa462287
Rob Cohen <rob.cohen@iohk.io>
2020-06-12 11:07:42 -0500

    fix

--- source/conf.py
+++ source/conf.py
@@ -62,3 +62,12 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+  "display_github": True, # Add 'Edit on Github' link instead of 'View page source'
+  "github_user": "input-output-hk",
+  "github_repo": technical-docs,
+  "github_version": "master",
+  "conf_py_path": "/source/",
+  "source_suffix": source_suffix,
+}
