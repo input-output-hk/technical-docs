ccdc8efe73a507c78378229386bf097a438097dc
Rob Cohen <robcohen@users.noreply.github.com>
2020-06-17 15:21:16 -0500

    Update git-clone.sh

--- git-clone.sh
+++ git-clone.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone --branch "pre-build" https://$(GIT_PAT)@github.com/input-output-hk/technical-docs.git source
-
 while IFS=" " read -r repo dest remainder; do
     git clone --branch "robcohen/docs" "$repo" "$dest"
 done < repo-list.txt
