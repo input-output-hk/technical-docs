a6d0d0880652a47e27056f54fa625d1c581b5930
Rob Cohen <rob@robcohen.dev>
2020-06-17 09:44:25 -0500

    fix

--- git-clone.sh
+++ git-clone.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git clone https://github.com/input-output-hk/technical-docs.git source
+
 while IFS=" " read -r repo dest remainder; do
     git clone --branch "robcohen/docs" "$repo" "$dest"
 done < repo-list.txt
