64bf106eb60f2637ee9ec76dfcd8807181c50daf
Rob Cohen <rob@robcohen.dev>
2020-06-22 10:24:56 -0500

    fix

--- git-clone.sh
+++ git-clone.sh
@@ -2,6 +2,7 @@
 
 # Clone cardano-tutorials
 git clone --depth=1 --branch "robcohen/docs" https://github.com/input-output-hk/cardano-tutorials.git source/cardano-tutorials
+rm -rf source/cardano-tutorials/.git
 
 while IFS=" " read -r name repo remainder; do
     git clone --depth=1 --branch "robcohen/docs" "$repo" "git/$name"
