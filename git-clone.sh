d17360578d7d3f7196d1a31360a11db55cbaf6ed
Rob Cohen <rob@robcohen.dev>
2020-06-22 12:28:03 -0600

    fix

--- git-clone.sh
+++ git-clone.sh
@@ -6,6 +6,10 @@ rm -rf source/cardano-tutorials/.git
 
 while IFS=" " read -r name repo remainder; do
     git clone --depth=1 --branch "robcohen/docs" "$repo" "git/$name"
+    if [[ -d "source/cardano-components/$name" ]]
+    then
+        rm -rf "source/cardano-components/$name"
+    fi
     mkdir -p "source/cardano-components/$name"
     mv "git/$name/doc" "source/cardano-components/$name/"
     mv "git/$name/README.rst" "source/cardano-components/$name/"
