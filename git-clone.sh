d88a63463833002952b1f5111fa38fbbb121d845
Rob Cohen <rob@robcohen.dev>
2020-06-22 10:02:29 -0500

    fix

--- git-clone.sh
+++ git-clone.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-while IFS=" " read -r repo dest remainder; do
-    git clone --branch "robcohen/docs" "$repo" "$dest"
+
+# Clone cardano-tutorials
+git clone --depth=1 --branch "robcohen/docs" https://github.com/input-output-hk/cardano-tutorials.git source/cardano-tutorials
+
+while IFS=" " read -r name repo remainder; do
+    git clone --depth=1 --branch "robcohen/docs" "$repo" "git/$name"
+    mkdir -p "source/cardano-components/$name"
+    mv "git/$name/doc" "source/cardano-components/$name/doc"
+    mv "git/$name/README.rst" "source/cardano-components/$name/"
+    rm -rf git
 done < repo-list.txt
--- source/cardano-components.rst
+++ source/cardano-components.rst
@@ -3,3 +3,10 @@ Cardano Components
 ==================
 
 This repo contains the technical documentation for the individual repos for the Cardano project.
+
+.. toctree::
+   :maxdepth: 3
+   :titlesonly:
+   :hidden:
+
+   adrestia/doc/index
