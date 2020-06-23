6990f570ed0a4c1605c4e70188bf7b9639461c20
Rob Cohen <rob@robcohen.dev>
2020-06-23 12:46:05 -0600

    fix

--- source/conf.py
+++ source/conf.py
@@ -65,6 +65,8 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_logo = "cardano-logo.png"
+
 html_context = {
   "display_github": True, # Add 'Edit on Github' link instead of 'View page source'
   "github_user": "input-output-hk",
