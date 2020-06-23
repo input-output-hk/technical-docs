ce53ee729d865ecabaf534da3a154db4f5a9ebe0
Rob Cohen <rob@robcohen.dev>
2020-06-23 14:36:37 -0600

    fix

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
+import recommonmark
+from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
 
