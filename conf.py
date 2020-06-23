8ebfcae277283c385c2f798923c548a29bac52d0
Github-Robot <github-robot@users.noreply.github.com>
2020-06-23 13:57:30 +0000

    Update

--- conf.py
+++ conf.py
@@ -34,7 +34,8 @@ master_doc = 'index'
 extensions = [
     "sphinx_rtd_theme",
     'sphinxcontrib.mermaid',
-    'recommonmark'
+    'recommonmark',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
