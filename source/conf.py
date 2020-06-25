303c4743065ef1b4824b81d30123781c8abc8936
Rob Cohen <rob@robcohen.dev>
2020-06-25 08:15:46 -0600

    fix

--- source/conf.py
+++ source/conf.py
@@ -43,6 +43,7 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+html_static_path = ['_static']
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -80,7 +81,6 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 html_logo = "cardano-logo.png"
 
